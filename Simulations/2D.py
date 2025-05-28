1e4f67b8b27df62cee7e4aefdfd6f6d3e5d8726d
Nakul Sharma <nakulsharmas2005@gmail.com>
2025-05-28 23:02:41 +0530

    2d

--- Simulations/2D.py
+++ Simulations/2D.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate as sci
 
 # eq of motions
-# F = M*a = M*z"  z : altitude
+# F = M*a = M*y"  y : altitude
 # // the notations are as follows :
 # a' means the first derivative of the value wrt time and a" means the double derivative
 # i will add the eq as we need to add in the sims but will show in comments the actual ways they are being used
@@ -13,96 +13,93 @@ Rplanet = 6378e3  # m
 # here we will simulate the launch in 2D
 mass = 1
 
-#we will be using the zx plane as in the z axs is the y-axis and the x-axis is the same , 
+#we will be using the yx plane as in the y axs is the y-axis and the x-axis is the same , 
 # think of it as slicing the earth from the middle
 
-# see Z.png for diagram
+# see y.png for diagram
 
-def gravity(x,z):
+def gravity(x,y):
     global G
     global M_e, Rplanet
-    r = np.sqrt(x**2 + z**2)
+    r = np.sqrt(x**2 + y**2)
     if r < Rplanet:
         accelX = 0
-        accelZ = 0
-        return accelX,accelZ
+        accely = 0
+        return np.asarray([accelX,accely])
     accelX = ((G*M_e)/r**3 )*x
-    accelZ = ((G*M_e)/r**3 )*z
-    return accelX,accelZ
+    accely = ((G*M_e)/r**3 )*y
+    return np.asarray([accelX,accely])
 
 
 # second order differnetial
 def derivatives(state, t):
     # state vector will have 2 rows
-    z = state[0]
+    y = state[0]
     x =state[1]
-    velz = state[2]
+    vely = state[2]
     velx = state[3]
     global mass
-    # compute zdot - Kinematic rel
-    zdot = velz
+    # compute ydot - Kinematic rel
+    ydot = vely
     xdot = velx
-    # Total forces
-    accelx , accelz = gravity(x,z)
     # Gravity
-    F_gx = -accelx * mass
-    F_gz = -accelz * mass
-    F_gT = np.sqrt(F_gz**2 + F_gx**2)
+    GravityF = -gravity(x,y)*mass
     # Aerodynamic
-    aero = 0
+    aero = np.asarray([0.0,0.0])
     # Thrust
-    thrust = 0
+    thrust = np.asarray([0.0,0.0])
 
-    Forces = F_g + aero + thrust
+    Forces = GravityF + aero + thrust
 
-    # zddot
-    zddot = Forces / mass
+    # yddot
+    ddot = Forces / mass
 
     # state' vector needs to be calculated
-    statedot = np.asarray([zdot, zddot])
+    statedot = np.asarray([ydot,xdot, ddot[1],ddot[0]])
 
     return statedot
 
 
 ## Main Script
 
-tout = np.linspace(
-    0, 30000, 1000
-)  # np.linespace(start ,end , n) : this function allows you to make an array with the starting and ending points with n no of points in between including the endpoints
-
 #Input parmas 
-x0 = Rplanet    
-z0 = Rplanet  # m
-velz_0 = 331  # m/s
+x0 = Rplanet + 40000   
+y0 = 0  # m
+vely_0 = 8000 # m/s
 velx_0 = 0 #m/s
-stateinitial = np.asarray([z0,x0 , velz_0,velx_0])
+r0 = np.sqrt(x0**2 + y0**2)
+TimePeriod = 2*np.pi/np.sqrt(G*M_e/r0)
+tout = np.linspace(
+    0, TimePeriod, 1000
+)  # np.linespace(start ,end , n) : this function allows you to make an array with the starting and ending points with n no of points in between including the endpoints
+stateinitial = np.asarray([y0,x0,vely_0,velx_0])
 
 # Outputs 
 stateout = sci.odeint(derivatives, stateinitial, tout)
-zout = stateout[:, 0]
+yout = stateout[:, 0]
 xout = stateout[:, 1]
-velz_out = stateout[:, 2]
+vely_out = stateout[:, 2]
 velx_out = stateout[:, 3]
-altitude = np.sqrt(xout**2 + zout**2) - Rplanet
-vel_out = np.sqrt(velx_out**2 + velz_out**2)
+altitude = np.sqrt(xout**2 + yout**2) - Rplanet
+vel_out = np.sqrt(velx_out**2 + vely_out**2)
 # plottings 
 theta = np.linspace(0,2*np.pi,360)
 xplanet = Rplanet*np.sin(theta)
 yplanet = Rplanet*np.cos(theta)
-plt.figure()
-plt.plot(altitude,tout)
-plt.title("Altitude")
-plt.grid()
-plt.show()
+# plt.figure()
+# plt.plot(altitude,tout)
+# plt.title("Altitude")
+# plt.grid()
+# plt.show()
 
-plt.figure()
-plt.title("Speed")
-plt.plot(vel_out,tout)
-plt.grid()
+# plt.figure()
+# plt.title("Speed")
+# plt.plot(vel_out,tout)
+# plt.grid()
 
 plt.figure()
 plt.plot(xplanet,yplanet)
-plt.plot(xout)
-plt.plot(zout)
+plt.plot(xout,yout)
+
 plt.grid()
 plt.show()
